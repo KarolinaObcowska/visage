c516f6bb61f4db634aacfb71fe4ff4ff4dbcadf4
Karolina Obcowska <k.obcowska@sellintegro.com>
2023-11-03 09:15:00 +0100

    fixed next.config.js configuration: replaced exportPathMap -> generateStaticParams

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  exportPathMap: async function () {
+  generateStaticParams: async function () {
     return {
       '/': { page: '/login' },
     }
