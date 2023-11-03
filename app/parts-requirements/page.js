3e68144e63b4ba1cf2d397503f5ec9f4e6cebe1c
Karolina Obcowska <k.obcowska@sellintegro.com>
2023-11-03 13:15:29 +0100

    +displaying csv data

--- app/parts-requirements/page.js
+++ app/parts-requirements/page.js
@@ -1,12 +1,36 @@
 import Table from '@/components/Table/Table'
+import * as fs from 'fs'
+import * as XLSX from 'xlsx'
 
-const PartsRequirements = () => {
+async function getData() {
+  const excelData = fs.readFileSync('./public/products.xlsx', (err) => {
+    if (err) {
+      console.log(err.message)
+      throw err
+    }
+    console.log('data written to file')
+  })
+  const workbook = XLSX.read(excelData, { type: 'buffer' })
+  const sheetName = workbook.SheetNames[0]
+  const sheet = workbook.Sheets[sheetName]
+  const data = XLSX.utils.sheet_to_json(sheet)
+  const groupedData = {}
+  data.forEach((row) => {
+    const group = row['Nazwa grupy']
+    if (!groupedData[group]) {
+      groupedData[group] = []
+    }
+    groupedData[group].push(row)
+  })
+  return groupedData
+}
+
+const PartsRequirements = async () => {
+  const data = await getData()
+
   return (
     <div className="w-full h-full bg-base-100 py-10 px-12">
-      <Table />
-      <div className="w-full flex justify-end my-6">
-        <button className="btn btn-success">Wyślij</button>
-      </div>
+      <Table data={data} />
     </div>
   )
 }
--- components/Table/Table.js
+++ components/Table/Table.js
@@ -1,69 +1,81 @@
-import React from 'react'
+'use client'
+import { BsChevronDown } from 'react-icons/bs'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
-const Table = () => {
+const Table = ({ data }) => {
+  const [expandedGroups, setExpandedGroups] = useState([])
+
+  const { register, handleSubmit } = useForm()
+
+  const expandRow = (e, group) => {
+    e.preventDefault()
+    if (expandedGroups.includes(group)) {
+      setExpandedGroups(expandedGroups.filter((g) => g !== group))
+    } else {
+      setExpandedGroups([...expandedGroups, group])
+    }
+  }
+
+  const submitForm = async (data) => {
+    console.log(data)
+  }
   return (
     <div>
-      <div class="relative overflow-x-auto shadow-md ">
-        <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-base-100 uppercase bg-primary ">
-            <tr>
-              <th scope="col" class="px-6 py-4">
-                Zdjęcie
-              </th>
-              <th scope="col" class="px-6 py-4">
-                Nazwa
-              </th>
-              <th scope="col" class="px-6 py-4">
-                Kod
-              </th>
-              <th scope="col" class="px-6 py-4">
-                Kod
-              </th>
-              <th scope="col" class="px-6 py-4">
-                Ilość
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="bg-white border-b">
-              <td class="px-6 py-4 bg-white font-medium text-gray-900 whitespace-nowrap dark:text-white flex items-center justify-center">
-                <img src="/img/example.jpg" className="w-16" />
-              </td>
-              <td class="px-6 py-4">Test</td>
-              <td class="px-6 py-4">123456</td>
-              <td class="px-6 py-4">123456</td>
+      <form class="relative  flex flex-col items-center " onSubmit={handleSubmit(submitForm)}>
+        <div className="w-3/4">
+          <table class="w-full text-sm text-center text-gray-500 text-gray-400">
+            <thead class="text-xs text-base-100 uppercase bg-primary w-full ">
+              <tr>
+                <th scope="col" class="px-6 py-4 w-1/4">
+                  Indeks
+                </th>
+                <th scope="col" class="px-6 py-4 w-1/4">
+                  Opis towaru
+                </th>
+                <th scope="col" class="px-6 py-4 w-1/4">
+                  Jednostka miary
+                </th>
+                <th scope="col" class="px-6 py-4 w-1/8">
+                  Ilość
+                </th>
+              </tr>
+            </thead>
+            {Object.keys(data).map(
+              (group) =>
+                group !== 'undefined' && (
+                  <tbody key={group} className="w-full bg-white">
+                    <tr colSpan={4} className=" border border-white z-2" key={group}>
+                      <th colSpan={4} onClick={(e) => expandRow(e, group)} class="bg-white px-6 py-4 border-b cursor-pointer text-left">
+                        <p className="flex items-center gap-6">
+                          <p className={expandedGroups.includes(group) && ` text-secondary`}>{group}</p>
+                          <BsChevronDown className={expandedGroups.includes(group) && `rotate-180 text-secondary`} />
+                        </p>
+                      </th>
+                    </tr>
 
-              <td class="px-6 py-4">
-                <input className="input input-bordered h-10 bg-white" type="number" />
-              </td>
-            </tr>
-            <tr class="bg-white border-b   ">
-              <td scope="row" class="px-6 py-4 bg-white font-medium text-gray-900 whitespace-nowrap dark:text-white flex items-center justify-center">
-                <img src="/img/example.jpg" className="w-16" />
-              </td>
-              <td class="px-6 py-4">Test</td>
-              <td class="px-6 py-4">123456</td>
-              <td class="px-6 py-4">123456</td>
-
-              <td class="px-6 py-4">
-                <input className="input input-bordered h-10 bg-white" type="number" />
-              </td>
-            </tr>
-            <tr class="bg-white border-b  ">
-              <td scope="row" class="px-6 py-4 bg-white font-medium text-gray-900 whitespace-nowrap dark:text-white flex items-center justify-center">
-                <img src="/img/example.jpg" className="w-16" />
-              </td>
-              <td class="px-6 py-4">Test</td>
-              <td class="px-6 py-4">123456</td>
-              <td class="px-6 py-4">123456</td>
-
-              <td class="px-6 py-4">
-                <input className="input input-bordered h-10 bg-white" type="number" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+                    {expandedGroups.includes(group) &&
+                      data[group].map((el) => (
+                        <tr key={el} className="bg-base-200 text-gray-600 border-t-2 border-r border-l border-base-100 z-1">
+                          <td className="w-1/4">{el.Indeks}</td>
+                          <td className="w-1/4">{el['Opis towaru']}</td>
+                          <td className="w-1/4">{el['JM']}</td>
+                          <td class="px-6 py-2 w-1/8">
+                            <input className="input input-bordered h-8 w-32 bg-red" type="number" defaultValue={0} {...register(el.Indeks)} />
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                )
+            )}
+          </table>
+        </div>
+        <div className="w-full flex justify-end my-6">
+          <button className="btn btn-success" type="submit">
+            Wyślij
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
